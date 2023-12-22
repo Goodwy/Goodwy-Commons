fe3943539f82e0ae2068d59c2138b91eb5ded545
Goodwy <goodwy.dev@gmail.com>
2023-12-22 17:01:22 +0300

    Update ContactsHelper.kt

--- commons/src/main/kotlin/com/goodwy/commons/helpers/ContactsHelper.kt
+++ commons/src/main/kotlin/com/goodwy/commons/helpers/ContactsHelper.kt
@@ -93,6 +93,7 @@ class ContactsHelper(val context: Context) {
 
             Contact.sorting = context.baseConfig.sorting
             Contact.startWithSurname = context.baseConfig.startNameWithSurname
+            Contact.sortingSymbolsFirst = context.baseConfig.sortingSymbolsFirst
             resultContacts.sort()
 
             Handler(Looper.getMainLooper()).post {
