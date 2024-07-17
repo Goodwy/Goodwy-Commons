71abdaf24ec4a0cb2e07467001db3341c69f837c
Goodwy <goodwy.dev@gmail.com>
2024-07-17 17:33:08 +0300

    Add contactThumbnailsSize to adapter

--- commons/src/main/kotlin/com/goodwy/commons/adapters/MyRecyclerViewListAdapter.kt
+++ commons/src/main/kotlin/com/goodwy/commons/adapters/MyRecyclerViewListAdapter.kt
@@ -33,6 +33,7 @@ abstract class MyRecyclerViewListAdapter<T>(
     protected var backgroundColor = activity.getProperBackgroundColor()
     protected var properPrimaryColor = activity.getProperPrimaryColor()
     protected var contrastColor = properPrimaryColor.getContrastColor()
+    protected var contactThumbnailsSize = activity.baseConfig.contactThumbnailsSize
     protected var actModeCallback: MyActionModeCallback
     protected var selectedKeys = LinkedHashSet<Int>()
     protected var positionOffset = 0
