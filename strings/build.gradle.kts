a964c6b0cc27d2660ecad5d5c8fa81ef4804629d
Goodwy <goodwy.dev@gmail.com>
2024-07-31 23:54:11 +0300

    Update build.gradle.kts

--- strings/build.gradle.kts
+++ strings/build.gradle.kts
@@ -1,8 +1,24 @@
 plugins {
     alias(libs.plugins.library)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.parcelize)
+    `maven-publish`
 }
+
+group = "com.goodwy"
+version = "5.2.0"
 
 android {
     namespace = "com.goodwy.commons.strings"
     compileSdk = libs.versions.app.build.compileSDKVersion.get().toInt()
+}
+
+publishing.publications {
+    create<MavenPublication>("release") {
+        afterEvaluate {
+            from(components["release"])
+        }
+    }
 }
