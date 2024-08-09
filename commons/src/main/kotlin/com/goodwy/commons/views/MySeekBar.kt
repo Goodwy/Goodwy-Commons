5417ef01da45598542a9ce06ab8ab453638dc3c7
Goodwy <goodwy.dev@gmail.com>
2024-08-09 19:54:23 +0300

    Update MySeekBar.kt

--- commons/src/main/kotlin/com/goodwy/commons/views/MySeekBar.kt
+++ commons/src/main/kotlin/com/goodwy/commons/views/MySeekBar.kt
@@ -3,7 +3,6 @@ package com.goodwy.commons.views
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
-import android.widget.SeekBar
 import androidx.appcompat.widget.AppCompatSeekBar
 import com.goodwy.commons.extensions.applyColorFilter
 
@@ -15,8 +14,8 @@ class MySeekBar : AppCompatSeekBar {
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
 
     fun setColors(textColor: Int, accentColor: Int, backgroundColor: Int = Color.WHITE) {
-        progressDrawable.applyColorFilter(accentColor)
-        background.applyColorFilter(textColor)
+        progressDrawable?.applyColorFilter(accentColor)
+        background?.applyColorFilter(textColor)
         thumb?.applyColorFilter(backgroundColor)
     }
 }
