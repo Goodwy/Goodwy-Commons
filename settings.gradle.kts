afb56b90b335a0e17c12daf8c2f882990ca183a2
Goodwy <goodwy.dev@gmail.com>
2025-07-31 00:39:19 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,17 +8,17 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
-    val isRuStoreBuild = gradle.startParameter.taskRequests
-        .any { it.args.any { arg -> arg.contains("rustore", ignoreCase = true) } }
+//    val isRuStoreBuild = gradle.startParameter.taskRequests
+//        .any { it.args.any { arg -> arg.contains("rustore", ignoreCase = true) } }
 
     repositories {
         google()
         mavenCentral()
         maven { setUrl("https://jitpack.io") }
 
-        if (isRuStoreBuild) {
+//        if (isRuStoreBuild) {
             maven { setUrl("https://artifactory-external.vkpartner.ru/artifactory/maven") }
-        }
+//        }
     }
 }
 rootProject.name = "Goodwy-Commons"
