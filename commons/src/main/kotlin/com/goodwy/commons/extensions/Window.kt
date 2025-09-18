540d460570046964615a9660471357d99416407a
Goodwy <goodwy.dev@gmail.com>
2025-09-18 22:27:07 +0300

    Update Window.kt

--- commons/src/main/kotlin/com/goodwy/commons/extensions/Window.kt
+++ commons/src/main/kotlin/com/goodwy/commons/extensions/Window.kt
@@ -3,29 +3,40 @@ package com.goodwy.commons.extensions
 import android.view.View
 import android.view.Window
 import com.goodwy.commons.helpers.DARK_GREY
-import com.goodwy.commons.helpers.isOreoPlus
 
 fun Window.updateStatusBarColors(backgroundColor: Int) {
+    if (statusBarColor == backgroundColor) {
+        return
+    }
     statusBarColor = backgroundColor
     updateStatusBarForegroundColor(backgroundColor)
 }
 
 fun Window.updateStatusBarForegroundColor(backgroundColor: Int) {
-    if (backgroundColor.getContrastColor() == DARK_GREY) {
-        decorView.systemUiVisibility = decorView.systemUiVisibility.addBit(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
-    } else {
-        decorView.systemUiVisibility = decorView.systemUiVisibility.removeBit(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+    val shouldBeLight = backgroundColor.getContrastColor() == DARK_GREY
+
+    decorView.post {
+        if (shouldBeLight) {
+            decorView.systemUiVisibility = decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            decorView.systemUiVisibility = decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        }
     }
 }
 
 fun Window.updateNavigationBarColors(backgroundColor: Int) {
+    if (navigationBarColor == backgroundColor) {
+        return
+    }
     navigationBarColor = backgroundColor
     updateNavigationBarForegroundColor(backgroundColor)
 }
 
 fun Window.updateNavigationBarForegroundColor(backgroundColor: Int) {
-    if (isOreoPlus()) {
-        if (backgroundColor.getContrastColor() == DARK_GREY) {
+    val shouldBeLight = backgroundColor.getContrastColor() == DARK_GREY
+
+    decorView.post {
+        if (shouldBeLight) {
             decorView.systemUiVisibility = decorView.systemUiVisibility.addBit(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
         } else {
             decorView.systemUiVisibility = decorView.systemUiVisibility.removeBit(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
