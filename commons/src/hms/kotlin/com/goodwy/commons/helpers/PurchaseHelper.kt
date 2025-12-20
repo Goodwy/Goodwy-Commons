2347d135903243c436de6817617bd32dc77d789d
Goodwy <goodwy.dev@gmail.com>
2025-12-20 17:22:36 +0300

    Update PurchaseHelper.kt

--- commons/src/hms/kotlin/com/goodwy/commons/helpers/PurchaseHelper.kt
+++ commons/src/hms/kotlin/com/goodwy/commons/helpers/PurchaseHelper.kt
@@ -1,8 +1,17 @@
 package com.goodwy.commons.helpers
 
 import android.app.Application
+import android.content.Context
+import android.content.Intent
 import com.goodwy.commons.activities.BaseSimpleActivity
 import com.goodwy.commons.extensions.baseConfig
+import com.goodwy.commons.extensions.toast
+import com.goodwy.commons.helpers.AppUpdateHelper.isHmsAvailable
+import com.huawei.hms.jos.AppUpdateClient
+import com.huawei.hms.jos.JosApps
+import com.huawei.updatesdk.service.appmgr.bean.ApkUpgradeInfo
+import com.huawei.updatesdk.service.otaupdate.CheckUpdateCallBack
+import com.huawei.updatesdk.service.otaupdate.UpdateKey
 
 class PurchaseHelper {
     fun initPurchaseIfNeed(app: Application, id: String) {
@@ -23,6 +32,16 @@ class PurchaseHelper {
         ),
         callback: (updatePro: Boolean) -> Unit,
     ) {
+        // Check Update
+        val client = JosApps.getAppUpdateClient(activity)
+
+        if(isHmsAvailable(activity) == 0){ // Check if Huawei Mobile Services available (0 for available) --> Update HMS Core if not available
+            client.checkAppUpdate(activity, UpdateCallBack(activity))
+        }
+        else {
+            activity.toast("HMS not available")
+        }
+
         val hmsHelper = HmsHelper(activity)
         hmsHelper.initBillingClient()
         hmsHelper.retrieveDonation(iapList, subList)
@@ -53,6 +72,57 @@ class PurchaseHelper {
                 }
                 is Tipping.FailedToLoad -> {}
             }
+        }
+    }
+
+    /**
+     * Class for handling the response after checking if an update is available
+     * @param[context] Context for this function
+     * @return handler for the response after checking if an update is available
+     * @author samlach2222
+     */
+    private class UpdateCallBack(private var context: Context) : CheckUpdateCallBack {
+        val client: AppUpdateClient = JosApps.getAppUpdateClient(context)
+
+        /**
+         * function for starting the update
+         * @param[intent] Intent used by the update
+         * @author samlach2222
+         */
+        override fun onUpdateInfo(intent: Intent?) {
+            intent?.let {
+                // Get the status
+                val status = it.getIntExtra(UpdateKey.STATUS, -1)
+
+                // Get error code and error message from extras
+                val errorCode = it.getIntExtra(UpdateKey.FAIL_CODE, -1) // if you want to handle it
+                val errorMessage = it.getStringExtra(UpdateKey.FAIL_REASON) // if you want to handle it
+
+                // Get the info as serializable
+                val info = it.getSerializableExtra(UpdateKey.INFO)
+
+                // If info is an instance of ApkUpgradeInfo, there is an update available
+                if (info is ApkUpgradeInfo) {
+
+                    //Show update dialog with force update type (true/false)
+                    client.showUpdateDialog(context, info, false)
+                }
+                else {
+                    context.toast("No updates available")
+                }
+            }
+        }
+
+        override fun onMarketInstallInfo(intent: Intent?) {
+
+        }
+
+        override fun onMarketStoreError(errorCode: Int) {
+
+        }
+
+        override fun onUpdateStoreError(errorCode: Int) {
+
         }
     }
 }
