74b852248035169a6089178ce96f0f2743f2dfb3
Goodwy <goodwy.dev@gmail.com>
2026-02-18 17:24:13 +0300

    Update Event.kt

--- commons/src/main/kotlin/com/goodwy/commons/models/contacts/Event.kt
+++ commons/src/main/kotlin/com/goodwy/commons/models/contacts/Event.kt
@@ -1,6 +1,32 @@
 package com.goodwy.commons.models.contacts
 
 import kotlinx.serialization.Serializable
+import java.util.Objects
 
 @Serializable
-data class Event(var value: String, var type: Int, var label: String)
+data class Event(
+    var value: String? = null,
+    var type: Int = 0,
+    var label: String? = null
+) {
+    override fun hashCode(): Int {
+        return Objects.hash(value, type, label)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Event
+
+        if (type != other.type) return false
+        if (value != other.value) return false
+        if (label != other.label) return false
+
+        return true
+    }
+
+    override fun toString(): String {
+        return "Event(value=$value, type=$type, label=$label)"
+    }
+}
