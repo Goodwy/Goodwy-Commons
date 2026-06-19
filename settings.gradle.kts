e4f261c775d8a8eb80b617d76645f741bc757dc6
Goodwy <goodwy.dev@gmail.com>
2026-06-20 01:13:17 +0300

    Fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ dependencyResolutionManagement {
         maven { setUrl("https://jitpack.io") }
         maven { setUrl("https://developer.huawei.com/repo/") }
         maven { setUrl("https://artifactory-external.vkpartner.ru/artifactory/maven") }
+        maven { setUrl("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
     }
 }
 rootProject.name = "Goodwy-Commons"
